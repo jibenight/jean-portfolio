b90546ac0d78399b19f61bd80a3edbb45f902d1e
Jean <nguyen.jean@me.com>
2021-03-30 19:07:30 +0200

    add js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
+let cancel = document.getElementById('cancel');
+let home = document.getElementById('home');
+let header = document.querySelector('header');
+let footer = document.querySelector('footer');
+
 // cancel icon
-document.getElementById('cancel').addEventListener('click', function () {
-  document.getElementById('home').style.display = 'flex';
-  document.getElementById('cancel').style.display = 'none';
-  document.querySelector('header').style.display = 'block';
-  document.querySelector('footer').style.display = 'none';
+cancel.addEventListener('click', function () {
+  home.style.display = 'flex';
+  cancel.style.display = 'none';
+  header.style.display = 'block';
+  footer.style.display = 'none';
   document.getElementById('aboutMe').style.display = 'none';
 });
 
 // nav feature
-document.getElementById('nav-about').addEventListener('click', function () {
-  document.getElementById('home').style.display = 'none';
-  document.getElementById('cancel').style.display = 'block';
-  document.querySelector('header').style.display = 'none';
-  document.querySelector('footer').style.display = 'flex';
-  document.getElementById('aboutMe').style.display = 'block';
+// document.getElementById('nav-about').addEventListener('click', function () {
+//   home.style.display = 'none';
+//   cancel.style.display = 'block';
+//   header.style.display = 'none';
+//   footer.style.display = 'flex';
+//   document.getElementById('aboutMe').style.display = 'block';
+// });
+
+function changeNav() {
+  const aboutMe = document.getElementById('aboutMe');
+
+  if (aboutMe.firstChild.nodeValue == 'À propos de moi') {
+    console.log('ça marche');
+  }
+  console.log('ça marche');
+}
+
+const el = document.querySelector('nav');
+el.addEventListener('click', function (e) {
+  console.log(e.target);
 });
