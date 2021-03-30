e132754eaf8fc025efd5aee43a04a05e1f63a457
Jean <nguyen.jean@me.com>
2021-03-30 08:37:40 +0200

    js function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
+// cancel icon
+document.getElementById('cancel').addEventListener('click', function () {
+  document.getElementById('home').style.display = 'flex';
+  document.getElementById('cancel').style.display = 'none';
+  document.querySelector('header').style.display = 'block';
+  document.querySelector('footer').style.display = 'none';
+  document.getElementById('aboutMe').style.display = 'none';
+});
+
 // nav feature
 document.getElementById('nav-about').addEventListener('click', function () {
   document.getElementById('home').style.display = 'none';
+  document.getElementById('cancel').style.display = 'block';
+  document.querySelector('header').style.display = 'none';
+  document.querySelector('footer').style.display = 'flex';
+  document.getElementById('aboutMe').style.display = 'block';
 });
